f60fae8bb00649a744d384055e3bb725d08e5905
stevecl5 <steveleighton5@gmail.com>
2021-06-29 22:49:47 -0700

    remove unused imports

--- WorkbenchProject/src/main/java/edu/uwb/braingrid/workbenchdashboard/WorkbenchDashboard.java
+++ WorkbenchProject/src/main/java/edu/uwb/braingrid/workbenchdashboard/WorkbenchDashboard.java
@@ -1,7 +1,6 @@
 package edu.uwb.braingrid.workbenchdashboard;
 
 import java.io.IOException;
-import java.net.URL;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 import java.io.File;
@@ -17,7 +16,6 @@ import edu.uwb.braingrid.general.LoggerHelper;
 import edu.uwb.braingrid.workbenchdashboard.threads.RunInit;
 import edu.uwb.braingrid.workbenchdashboard.userModel.User;
 import edu.uwb.braingrid.workbenchdashboard.utils.SystemProperties;
-import org.apache.jena.ext.com.google.common.io.Resources;
 
 /**
  * Workbench Dashboard Application, contains main.
